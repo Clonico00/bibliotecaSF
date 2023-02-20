87a9c83e36abcff5b6b9e15150aaf0dc976ad9f5
Javier Martinez Garcia <79504901+Clonico00@users.noreply.github.com>
2023-02-20 01:39:22 +0100

    updates

--- src/Form/EjemplarType.php
+++ src/Form/EjemplarType.php
@@ -4,17 +4,39 @@ namespace App\Form;
 
 use App\Entity\Ejemplar;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class EjemplarType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('estado')
-            ->add('estanteria')
-            ->add('edicion')
+            ->add('estado', Texttype::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 1, 'max' => 255]),
+                    new Regex(['pattern' => '/^[a-zA-Z ]+$/'])
+                ]
+            ])
+            ->add('estanteria', Texttype::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 1, 'max' => 255]),
+                    new Regex(['pattern' => '/^[a-zA-Z ]+$/'])
+                ]
+            ])
+            ->add('edicion', Texttype::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 1, 'max' => 255]),
+                    new Regex(['pattern' => '/^[a-zA-Z ]+$/'])
+                ]
+            ])
             ->add('libro')
         ;
     }
